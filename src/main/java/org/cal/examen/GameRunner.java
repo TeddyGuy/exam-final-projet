0b41628d291fbdcda2c7c13a5599ad2e6f4c8a45
TeddyGuy <mediteddyguy@gmail.com>
2022-12-07 15:40:10 -0500

    magic number

---
+++ src/main/java/org/cal/examen/GameRunner.java
@@ -0,0 +1,37 @@
+package org.cal.examen;
+
+import java.util.Random;
+
+public class GameRunner {
+
+    private static boolean notAWinner;
+    private static int MAX_ROLL_VALUE = 6;
+    private static int MIN_ROLL_VALUE = 1;
+
+
+
+    public static void main(String[] args) {
+        Game aGame = new Game();
+
+        aGame.add("Mathieu");
+        aGame.add("Samir");
+        aGame.add("Manolo");
+
+        Random rand = new Random();
+
+        do {
+
+            aGame.roll(rand.nextInt(MIN_ROLL_VALUE, MAX_ROLL_VALUE)); //Magic Number
+
+            if (rand.nextInt(9) == 7) {
+                notAWinner = aGame.wrongAnswer();
+            } else {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+
+
+
+        } while (notAWinner);
+
+    }
+}
